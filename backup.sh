e027cf97857912b687cf3c99f536a6f27632a8cb
marysokolova <mariasokolova2004@gmail.com>
2024-03-16 17:48:48 +0300

    Add 2 comments

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
-
+#This is bash script
 echo "The script you are running has:"
+#Команда basename в Linux печатает последний компонент в пути к файлу. 
 echo "basename: [$(basename "$0")]"
 echo "dirname : [$(dirname "$0")]"
+
 echo "pwd     : [$(pwd)]"
